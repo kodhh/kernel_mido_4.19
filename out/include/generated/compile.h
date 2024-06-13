ef6929ec50f1e046b76d7fbb85e59f11f6d83ed4
kodhh <limw94@outlook.com>
2024-06-13 22:21:58 +0800

    clean out

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#1 SMP PREEMPT Thu Jun 13 16:21:24 CST 2024"
-#define LINUX_COMPILE_BY "li"
-#define LINUX_COMPILE_HOST "li-VirtualBox"
-#define LINUX_COMPILER "Ubuntu clang version 12.0.0-3ubuntu1~20.04.5, LLD 12.0.0"
