faff0309d628049cf545997dd0ee2c92cd10ed6c
kodhh <limw94@outlook.com>
2024-06-14 11:32:45 +0800

    update config

--- drivers/power/supply/qcom/qpnp-smbcharger.c
+++ drivers/power/supply/qcom/qpnp-smbcharger.c
@@ -4430,24 +4430,32 @@ static int smbchg_restricted_charging(struct smbchg_chip *chip, bool enable)
 	return rc;
 }
 
-#ifdef CONFIG_MACH_XIAOMI_MIDO
+#if defined(CONFIG_MACH_XIAOMI_MIDO)
 extern void ist30xx_set_ta_mode(bool mode);
 extern void tpd_usb_plugin(bool mode);
 extern void gtp_usb_plugin(bool mode);
-int set_usb_charge_mode_par = 0;
 #endif
+#if defined(CONFIG_MACH_XIAOMI_MARKW)
+extern void gtp_usb_plugin(bool mode);
+#endif
+int set_usb_charge_mode_par = 0;
+
 static void handle_usb_removal(struct smbchg_chip *chip)
 {
 	struct power_supply *parallel_psy = get_parallel_psy(chip);
 	union power_supply_propval pval = {0, };
 	int rc;
 
-#ifdef CONFIG_MACH_XIAOMI_MIDO
+#if defined(CONFIG_MACH_XIAOMI_MIDO)
 	if (set_usb_charge_mode_par == 1)
 		ist30xx_set_ta_mode(0);
 	else if (set_usb_charge_mode_par == 2)
 		tpd_usb_plugin(0);
 	else if (set_usb_charge_mode_par == 3)
+		gtp_usb_plugin(0);
+#endif
+#if defined(CONFIG_MACH_XIAOMI_MARKW)
+	if (set_usb_charge_mode_par == 1)
 		gtp_usb_plugin(0);
 #endif
 
@@ -4511,7 +4519,7 @@ static bool is_usbin_uv_high(struct smbchg_chip *chip)
 }
 
 #define HVDCP_NOTIFY_MS		2500
-#ifdef CONFIG_MACH_XIAOMI_MIDO
+#if defined(CONFIG_MACH_XIAOMI_MIDO) || defined(CONFIG_MACH_XIAOMI_MARKW)
 static int rerun_apsd(struct smbchg_chip *chip);
 #endif
 static void handle_usb_insertion(struct smbchg_chip *chip)
@@ -4520,7 +4528,7 @@ static void handle_usb_insertion(struct smbchg_chip *chip)
 	int rc;
 	char *usb_type_name = "null";
 
-#ifdef CONFIG_MACH_XIAOMI_MIDO
+#if defined(CONFIG_MACH_XIAOMI_MIDO)
 	if (set_usb_charge_mode_par == 1)
 		ist30xx_set_ta_mode(1);
 	else if (set_usb_charge_mode_par == 2)
@@ -4528,11 +4536,15 @@ static void handle_usb_insertion(struct smbchg_chip *chip)
 	else if (set_usb_charge_mode_par == 3)
 		gtp_usb_plugin(1);
 #endif
+#if defined(CONFIG_MACH_XIAOMI_MARKW)
+	if (set_usb_charge_mode_par == 1)
+		gtp_usb_plugin(1);
+#endif
 
 	pr_smb(PR_STATUS, "triggered\n");
 	/* usb inserted */
 	read_usb_type(chip, &usb_type_name, &usb_supply_type);
-#ifdef CONFIG_MACH_XIAOMI_MIDO
+#if defined(CONFIG_MACH_XIAOMI_MIDO) || defined(CONFIG_MACH_XIAOMI_MARKW)
 	if (usb_supply_type == POWER_SUPPLY_TYPE_USB_CDP) {
 		rc = rerun_apsd(chip);
 		read_usb_type(chip, &usb_type_name, &usb_supply_type);
@@ -5842,7 +5854,7 @@ static int smbchg_battery_get_property(struct power_supply *psy,
 		val->intval = get_prop_batt_health(chip);
 		break;
 	case POWER_SUPPLY_PROP_TECHNOLOGY:
-#ifdef CONFIG_MACH_XIAOMI_MIDO
+#if defined(CONFIG_MACH_XIAOMI_MIDO) || defined(CONFIG_MACH_XIAOMI_MARKW)
 		val->intval = POWER_SUPPLY_TECHNOLOGY_LIPO;
 #else
 		val->intval = POWER_SUPPLY_TECHNOLOGY_LION;
@@ -6055,7 +6067,7 @@ static irqreturn_t batt_warm_handler(int irq, void *_chip)
 {
 	struct smbchg_chip *chip = _chip;
 	u8 reg = 0;
-#ifdef CONFIG_MACH_XIAOMI_MIDO
+#if defined(CONFIG_MACH_XIAOMI_MIDO) || defined(CONFIG_MACH_XIAOMI_MARKW)
 	int rc;
 
 	/* set the warm float voltage compensation,
@@ -6088,7 +6100,7 @@ static irqreturn_t batt_cool_handler(int irq, void *_chip)
 {
 	struct smbchg_chip *chip = _chip;
 	u8 reg = 0;
-#ifdef CONFIG_MACH_XIAOMI_MIDO
+#if defined(CONFIG_MACH_XIAOMI_MIDO) || defined(CONFIG_MACH_XIAOMI_MARKW)
 	int rc;
 
 	/* set the cool float voltage compensation,
@@ -7143,7 +7155,7 @@ static int smbchg_hw_init(struct smbchg_chip *chip)
 			dev_err(chip->dev, "Couldn't set OTG OC config rc = %d\n",
 				rc);
 
-#ifdef CONFIG_MACH_XIAOMI_MIDO
+#if defined(CONFIG_MACH_XIAOMI_MIDO) || defined(CONFIG_MACH_XIAOMI_MARKW)
 		rc = smbchg_sec_masked_write(chip, chip->otg_base + OTG_CFG,
 					     0x0c, 0x8);
 		if (rc < 0) {
@@ -8056,7 +8068,7 @@ static int smbchg_probe(struct platform_device *pdev)
 		goto votables_cleanup;
 	}
 
-#ifdef CONFIG_MACH_XIAOMI_MIDO
+#if defined(CONFIG_MACH_XIAOMI_MIDO) || defined(CONFIG_MACH_XIAOMI_MARKW)
 	chip->hvdcp_not_supported = true;
 #endif
 
